e5efc59180424824a5fcd95d7051491c459a8fcd
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2021-05-22 19:27:33 +0300

    Initial commit

---
+++ SwiftUI Challenges/SwiftUI Challenges/SwiftUI_ChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChallengesApp.swift
+//  SwiftUI Challenges
+//
+//  Created by Vladislav Kondrashkov on 22.05.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
